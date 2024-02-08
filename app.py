e1c061fb4465d5440ef999f5dc40da02e2667a58
gabrieldiniz <gabriel.diniz@fengbrasil.com.br>
2024-02-08 11:44:39 -0300

    feat Adding log levels description

---
+++ app.py
@@ -0,0 +1,11 @@
+from loguru import logger
+
+# Log levels ordenados por severidade e verbosidade crescente
+
+logger.trace("A trace message.")
+logger.debug("A debug message.")
+logger.info("An info message.")
+logger.success("A success message.")
+logger.warning("A warning message.")
+logger.error("An error message.")
+logger.critical("A critical message.")

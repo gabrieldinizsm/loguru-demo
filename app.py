a753736edb5b55e55446cc004160330ea2e898a6
gabrieldiniz <gabriel.diniz@fengbrasil.com.br>
2024-02-23 01:11:11 -0300

    feat: Add custom log decorator Add a custom decorator that logs some useful infos every function run such as elapsed time, exceptions, initial and final timstamp and much more

--- app.py
+++ app.py
@@ -3,9 +3,64 @@ import sys
 from faker import Faker
 import numpy as np
 import pandas as pd
+import json
+import time
+import traceback
+import platform
+import psutil
 
 
-@logger.catch(level="ERROR")
+def log_function_info(func):
+    def wrapper(*args, **kwargs):
+
+        start_time = time.time()
+        initial_timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+        exception_info = None
+
+        initial_memory = round(
+            float(psutil.virtual_memory().used) / (1024.0 ** 2))
+
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as e:
+            exception_info = {
+                "exception_type": type(e).__name__,
+                "exception_message": str(e),
+                "traceback": traceback.format_exc()
+            }
+            raise e
+        finally:
+            end_time = time.time()
+            final_timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+            elapsed_time = round(end_time - start_time, 4)
+
+            final_memory = round(
+                float(psutil.virtual_memory().used) / (1024.0 ** 2))
+
+            environment_info = {
+                "python_version": sys.version,
+                "operating_system": platform.system(),
+            }
+
+            log_data = {
+                "function_name": func.__name__,
+                "script_name": __file__,
+                "initial_timestamp": initial_timestamp,
+                "final_timestamp": final_timestamp,
+                "elapsed_time": elapsed_time,
+                "exceptions": exception_info,
+                "initial_memory_usage": initial_memory,
+                "final_memory_usage": final_memory,
+                "environment_info": environment_info,
+            }
+
+            logger.info(json.dumps(log_data, indent=4))
+
+    return wrapper
+
+
+@logger.catch('ERROR')
 def generate_fake_email() -> str:
     """ 
     Function to generate some fake emails.
@@ -19,7 +74,7 @@ def generate_fake_email() -> str:
     return faker
 
 
-@logger.catch(level="ERROR")
+@log_function_info
 def generate_fake_df(num_rows: int) -> pd.DataFrame:
     """
     Function to generate a mocked DataFrame with pre defined columns and random values.
@@ -64,13 +119,16 @@ def generate_fake_df(num_rows: int) -> pd.DataFrame:
         raise (e)
 
 
-if __name__ == '__main__':
-
-    logger.add(
-        sys.stderr, level="WARNING", serialize=True, backtrace=True)
+@log_function_info
+def main():
 
     np.random.seed(42)
 
     df = generate_fake_df(100)
+
+
+if __name__ == '__main__':
+
+    main()
 
     logger.success("Successfully terminated")

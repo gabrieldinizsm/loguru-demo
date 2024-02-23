7ee158b5adf5e2049f82834c934b6d396b95a51d
gabrieldiniz <gabriel.diniz@fengbrasil.com.br>
2024-02-22 21:24:00 -0300

    feat: generating dummy dataframe generating some random data with numpy inside of a pandas dataframe

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from loguru import logger
 import sys
 from faker import Faker
+import numpy as np
+import pandas as pd
 
 
 def generate_fake_email() -> str:
@@ -13,3 +15,54 @@ def generate_fake_email() -> str:
     fake = Faker()
     faker = fake.email()
     return faker
+
+
+def generate_fake_df(num_rows: int) -> pd.DataFrame:
+    """
+    Function to generate a mocked DataFrame with pre defined columns and random values.
+
+    The DataFrame consists in 6 columns:
+        string_column: String that will vary in apple, banana and grape
+        int_column: Int that varies from 1 to 100
+        float_column: Int that varies from 1.0 to 100.0
+        datetime_column: Datetime that goes from 2010-01-01' to '2024-12-31'
+        email_column: String containing a fake email
+            Example: johndoe@example.com
+
+    Args:
+        num_rows(int): An integer containing the number of rows of the DataFrame and its columns.
+
+    Returns:
+        pd.DataFrame: A pandas DataFrame with mocked values.
+    """
+
+    try:
+
+        data = {}
+
+        data['string_column'] = np.random.choice(
+            ['apple', 'banana', 'grape'], size=num_rows)
+
+        data['int_column'] = np.random.randint(1, 100, size=num_rows)
+
+        data['float_column'] = np.random.uniform(1.0, 100.0, size=num_rows)
+
+        data['datetime_column'] = pd.to_datetime(np.random.choice(
+            pd.date_range('2010-01-01', '2024-12-31'), size=num_rows))
+
+        data['email_column'] = [generate_fake_email()
+                                for _ in range(num_rows)]
+
+        return pd.DataFrame(data)
+
+    except Exception as e:
+        raise (e)
+
+
+if __name__ == '__main__':
+
+    np.random.seed(42)
+
+    df = generate_fake_df(100)
+
+    print(df.head())

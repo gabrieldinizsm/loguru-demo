f07f5fb0dcc97e4dd9956c582915db520d71aee2
gabrieldiniz <gabriel.diniz@fengbrasil.com.br>
2024-02-23 00:28:58 -0300

    feat: add catch decorators to functions

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 
 
+@logger.catch(level="ERROR")
 def generate_fake_email() -> str:
     """ 
     Function to generate some fake emails.
@@ -14,9 +15,11 @@ def generate_fake_email() -> str:
     """
     fake = Faker()
     faker = fake.email()
+
     return faker
 
 
+@logger.catch(level="ERROR")
 def generate_fake_df(num_rows: int) -> pd.DataFrame:
     """
     Function to generate a mocked DataFrame with pre defined columns and random values.
@@ -53,7 +56,7 @@ def generate_fake_df(num_rows: int) -> pd.DataFrame:
         data['email_column'] = [generate_fake_email()
                                 for _ in range(num_rows)]
 
-        logger.success("Successfully terminated")
+        logger.success(f"Function finalizada")
 
         return pd.DataFrame(data)
 
@@ -64,12 +67,10 @@ def generate_fake_df(num_rows: int) -> pd.DataFrame:
 if __name__ == '__main__':
 
     logger.add(
-        sys.stderr, format="{time} {level} {message}", level="DEBUG", serialize=True)
+        sys.stderr, level="WARNING", serialize=True, backtrace=True)
 
     np.random.seed(42)
 
     df = generate_fake_df(100)
-
-    print(df.head())
 
     logger.success("Successfully terminated")
